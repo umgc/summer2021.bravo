455d3d341febfb266433eb2e003b1e4469555c49
tyler <tyler.puschinsky@gmail.com>
2021-07-27 21:11:35 -0400

    Add catch statement to voicehelper.dart to allow app to start without porcupine.
    Add initial widget tests as examples to build off.

--- harkify/test/widget_test.dart
+++ harkify/test/widget_test.dart
@@ -1,13 +1,37 @@
+import 'package:Harkify/basemenudrawer.dart';
+import 'package:Harkify/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/main.dart';
 
 void main() {
-  testWidgets('widget test', (WidgetTester tester) async {
+  testWidgets('test mic prompt', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Placeholder test until our unit tests are complete
-    expect(find.text('Test'), findsNothing);
+    // Application should prompt the user to start recording
+    expect(find.text('Press the mic button to start'), findsOneWidget);
+
   });
+
+  testWidgets('test open menu', (WidgetTester tester) async {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        key: scaffoldKey,
+        drawer: BaseMenuDrawer(),
+      )
+    ));
+
+    scaffoldKey.currentState!.openDrawer();
+    await tester.pump();
+
+    // Drawer should contain View Notes option
+    expect(find.text("View Notes"), findsOneWidget);
+
+    // Drawer should contain Settings option
+    expect(find.text("Settings"), findsOneWidget);
+  });
+
 }
