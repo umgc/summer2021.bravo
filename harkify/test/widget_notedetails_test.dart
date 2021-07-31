e82c42e548320ea8aac9759a1f559b3e7ac2b18d
Ben Cushing <cushingbf@gmail.com>
2021-07-30 22:45:42 -0400

    Added unit test for NoteDetails widget

--- harkify/test/widget_notedetails_test.dart
+++ harkify/test/widget_notedetails_test.dart
@@ -1,11 +1,11 @@
 import 'package:Harkify/textnoteservice.dart';
-import 'package:Harkify/viewnotes.dart';
+import 'package:Harkify/notedetails.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
 
-/// Conduct unit tests of the ViewNotes widget
+/// Conduct unit tests of the NoteDetails widget
 void main() {
   /// The file system to use for all I/O operations without saving real files
   FileSystem fileSystem = MemoryFileSystem();
@@ -23,26 +23,23 @@ void main() {
     );
   }
 
-  testWidgets('test view notes screen', (WidgetTester tester) async {
+  testWidgets('test note details screen', (WidgetTester tester) async {
     prepForTests();
     // Build our app and trigger a frame.
-    await tester.pumpWidget(createWidgetForTesting(child: new ViewNotes()));
+    await tester.pumpWidget(createWidgetForTesting(child: new NoteDetails()));
 
     // Should see the progress indicator while text notes load
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
 
     await tester.pumpAndSettle();
 
-    // Application should show "View Notes" as title
-    expect(find.text("View Notes"), findsOneWidget);
+    // Application should show "Notes" as title
+    expect(find.text("Notes"), findsOneWidget);
 
-    // Application should show that no text notes have been saved
-    expect(
-        find.text(
-            "Uh-oh! It looks like you don't have any text notes saved. Try saving some notes first and come back here."),
-        findsOneWidget);
+    // Application should have one edit button
+    expect(find.byIcon(Icons.edit), findsOneWidget);
 
-    // Application should have one "+" icon button
-    expect(find.byIcon(Icons.add), findsOneWidget);
+    // Application should have one delete button
+    expect(find.byIcon(Icons.delete), findsOneWidget);
   });
 }
